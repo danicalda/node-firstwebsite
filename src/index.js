7de788027ad54296c844cfea47f83f2de4c549ce
danicalda <57199875+danicalda@users.noreply.github.com>
2021-04-01 11:52:45 +0200

    first commit
    new web page with node

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//settings
+app.set('port', 2000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+//middelwares
+
+//routes
+app.use(require('./routes/index'));
+
+//static files
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//listeninig server port
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
